05b39b4c04d3ca52577d85156cc8068cdb766030
Owen Price Skelly <21372141+OwenPriceSkelly@users.noreply.github.com>
2023-02-17 12:26:33 -0600

    clarify comments/ more sphinx-friendly docstrings

--- garden_ai/steps.py
+++ garden_ai/steps.py
@@ -63,12 +63,16 @@ class Step:
     input_info: str
         Human-readable description of the input data and/or relevant
         characteristics that *should* hold true for this step's input (e.g.
-        dimensions of a matrix or column names of a dataframe).
+        dimensions of a matrix or column names of a dataframe). Currently,
+        ``typing.get_type_hints(func, include_extras=True)`` is used for the
+        default.
 
     output_info: str
         Human-readable description of the output data and/or relevant
         characteristics that *will* hold true for this step's output (e.g.
-        dimensions of a matrix or column names of a dataframe).
+        dimensions of a matrix or column names of a dataframe). Currently,
+        ``typing.get_type_hints(func, include_extras=True)`` is used for the
+        default.
 
 
     authors: List[str]
@@ -86,12 +90,13 @@ class Step:
 
     Notes
     -----
-    We require annotations because we need ``Step``s to be composable functions in
-    their respective ``Pipeline``s. However, due to python's highly flexible
+    We require annotations because we need ``Step``s to be composable functions
+    in their respective ``Pipeline``s. However, due to python's highly flexible
     ``*args`` syntax, function composition is inherently ambiguous -- e.g. if we
-    wish to compose ``f`` with ``g`,` and ``g` returns a tuple of values, should that tuple be passed to ``f``
-    as a tuple (i.e. ``f(g(*args))``), or should it be unpacked as individual
-    arguments to ``f`` (i.e. ``f(*g(*args))``, noting the extra ``*``)?
+    wish to compose ``f`` with ``g`,` and ``g` returns a tuple of values, should
+    that tuple be passed to ``f`` as a tuple (i.e. ``f(g(*args))``), or should
+    it be unpacked as individual arguments to ``f`` (i.e. ``f(*g(*args))``,
+    noting the extra ``*``)?
 
     To resolve this ambiguity, we could either (a) restrict the set of acceptable
     callables to be only those with a single argument and a single return value, or
@@ -99,9 +104,12 @@ class Step:
     are likely to stick to single-input-single-output ``Step``s, we currently try
     to support (b) by composing steps together differently if ``g` seems to be
     returning an "argument tuple" for ``f`` as follows:
-        - ``g` must have annotations indicating the types within the tuple, e.g. ``def g(...) -> tuple[str, int, pd.DataFrame]``
-        - ``f`` must have exactly those argument annotations, e.g. ``def f(x: str, y: int, z: pd.DataFrame) -> ...``
-        - if both are true, compose by unpacking like ``f(*g(*args))``, otherwise ``f(g(*args))`` like any other Step.
+        - ``g` has a return annotation indicating the types within the tuple:
+            e.g. ``def g(...) -> tuple[str, int, pd.DataFrame]``
+        - ``f`` has exactly those argument annotations, in the same order:
+            e.g. ``def f(x: str, y: int, z: pd.DataFrame) -> ...``
+        - if both are true, compose ``f`` with ``g`` by **unpacking the tuple when it returns**:
+            e.g. ``f(*g(*args))`` (noting the extra ``*``), otherwise ``f(g(*args))`` - like any other Step.
     """
 
     func: Callable
@@ -126,16 +134,12 @@ class Step:
 
     def __call__(self, *args, **kwargs):
         # keep it simple: just pass input the underlying callable.
-        # (though if we were going to do it anywhere, here's where we'd add
-        # logic to check that the types are what we expected them to be at
-        # runtime. see also: pydantic validate_arguments for inspiration)
         return self.func(*args, **kwargs)
 
     @validator("func")
     def has_annotations(cls, f: Callable):
         sig = signature(f)
         # check that any positional arguments have annotations
-        # maybe: warn about kwargs if any?
         for p in sig.parameters.values():
             if p.annotation is Parameter.empty:
                 raise TypeError(
@@ -143,6 +147,7 @@ class Step:
                     "Please double check that the argument list is fully annotated.\n"
                     "See also: https://peps.python.org/pep-0484/#type-definition-syntax"
                 )
+        # check that return value has annotation
         if sig.return_annotation in {Signature.empty, None}:
             raise TypeError(
                 f"{f.__name__}'s definition is missing a return annotation, or returns None.\n"
@@ -153,15 +158,16 @@ class Step:
     @validator("func")
     def uses_Any(cls, f: Callable):
         sig = signature(f)
-        # check that any positional arguments have annotations
+        # check input args
         for p in sig.parameters.values():
             if p.annotation is typing.Any:
                 raise TypeError(
-                    f"In {f.__name__}'s definition, found `Any` annotating parameter {p}, \n "
+                    f"In {f.__name__}'s definition, found `Any` annotating the parameter {p}, \n "
                     "which would prevent us from verifying that steps would compose together correctly\n"
                     "before being published as a Pipeline. Please try again with a more descriptive type hint.\n"
                     "We use `beartype` to resolve type hints -- for a full list of supported annotations \n "
-                    "(including 3rd party type hints, like `numpy.typing`), see:\nhttps://github.com/beartype/beartype#compliance"
+                    "(including 3rd party type hints, like `numpy.typing`), see:\n "
+                    "https://github.com/beartype/beartype#compliance"
                 )
         if sig.return_annotation is typing.Any:
             raise TypeError(
@@ -169,30 +175,27 @@ class Step:
                 "which would prevent us from verifying that steps would compose together correctly\n"
                 "before being published as a Pipeline. Please try again with a more descriptive type hint.\n"
                 "We use `beartype` to resolve type hints -- for a full list of supported annotations \n "
-                "(including 3rd party type hints, like `numpy.typing`), see:\nhttps://github.com/beartype/beartype#compliance"
+                "(including 3rd party type hints, like `numpy.typing`), see:\n "
+                "https://github.com/beartype/beartype#compliance"
             )
         return f
 
-    def register(self):
-        """NOT IMPLEMENTED
-
-        This method could be used to "register" the Step with funcx
-        """
-        raise NotImplementedError
-
 
 def step(func: Callable = None, **kwargs):
-    """Helper: provide decorator interface/syntax sugar for ``Step``s."""
+    """Helper: provide ``@step(...)`` decorator for creation of ``Step``s."""
     # note:
-    # while the Step class itself could also be used as a decorator to create a
-    # basically identical object, it's not possible to also pass kwargs (like a
-    # title or author) to the Step constructor without something like this.
+    # The ``Step`` class itself could also technically be used as a decorator,
+    # but would run into trouble as soon as you tried passing any arguments:
+    # this definition means ``@step`` and ``@step(...)`` are equivalent decorators,
+    # because ``@Step`` and ``@Step(...)`` could not be.
     if func is not None:
+        # called like ``@step``
+        # (or ``my_func = step(my_func, **kwargs)``)
         data = {**kwargs, "func": func}
         return Step(**data)  # type: ignore
 
     else:
-
+        # called like ``@step(**kwargs)``
         def wrapper(f):
             data = {**kwargs, "func": f}
             return Step(**data)
