7505f5932bc63085aecb75198e4890e3c069070e
Owen Price Skelly <21372141+OwenPriceSkelly@users.noreply.github.com>
2023-02-22 14:47:28 -0600

    creates pipeline with "shortname" identifier,
    i.e. a valid python variable name for the scaffolded file.
    
    if one isn't provided, the title is (slugified? snakeified?) reformatted
    to be a valid identifier and is used instead.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,12 +3,17 @@ import sys
 from garden_ai.app.main import app
 from garden_ai.client import GardenClient
 from typer.testing import CliRunner
+import string
+import random
+from garden_ai.app.pipeline import validate_identifier
 
 runner = CliRunner()
 
 
 @pytest.mark.cli
-@pytest.mark.skipif(sys.version_info < (3, 8), reason="mocked call_args.kwargs breaks under 3.7")
+@pytest.mark.skipif(
+    sys.version_info < (3, 8), reason="mocked call_args.kwargs breaks under 3.7"
+)
 def test_garden_create(garden_all_fields, tmp_path, mocker):
     mock_client = mocker.MagicMock(GardenClient)
     mocker.patch("garden_ai.app.garden.GardenClient").return_value = mock_client
@@ -36,3 +41,15 @@ def test_garden_create(garden_all_fields, tmp_path, mocker):
     for key in kwargs:
         assert kwargs[key] == getattr(garden_all_fields, key)
     mock_client.put_local.assert_called_once()
+
+
+def test_pipeline_create(tmp_path, mocker):
+    mock_client = mocker.MagicMock(GardenClient)
+    mocker.patch("garden_ai.app.pipeline.GardenClient").return_value = mock_client
+    # TODO
+
+
+def test_validate_identifier():
+    possible_name = "".join(random.choices(string.printable, k=50))
+    valid_name = validate_identifier(possible_name)
+    assert valid_name.isidentifier()
